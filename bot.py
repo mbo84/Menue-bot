9a540f5bf789739d05bcf38ac0113a9543aadf73
mbo84 <mboehi84@gmail.com>
2026-04-07 18:04:05 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ KALENDERWOCHE = heute.strftime("%V")
 # --- 2. DEINE DATEN (HIER ANPASSEN) ---
 # Ersetze dies durch die echte Webadresse deiner Kantine
 BASIS_URL = "https://labocca.ch/wp-content/uploads/" 
-PDF_URL = f"{BASIS_URL}{JAHR}/{MONAT}/Tagesmenu-{KALENDERWOCHE}_Grossacker.pdf"
+PDF_URL = f"{BASIS_URL}{JAHR}/{MONAT}/Tagesmenue{KALENDERWOCHE}_Grossacker.pdf"
 
 # Deine Lieblingsgerichte
 SUCHBEGRIFFE = ["Bratwurst", "Cordon Bleu", "Cheeseburger"]
